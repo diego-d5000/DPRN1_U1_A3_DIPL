b3966793b348dfc5ff7f7295e438f2d8e549c6e2
diego-d5000 <diego-d@outlook.com>
2016-07-29 00:23:04 -0500

    Initial homework project

---
+++ Actividad3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic
+
+namespace Actividad3
+{
+	enum AreasPercentage : byte { Mercadotecnia = 20, QnA = 20, Production = 40, Sales = 20 };
+	enum MarketingProjectsPercentage : byte { Proyecto1_M = 40, Proyecto2_M = 30, Proyecto3_M = 30 };
+	enum QnAProjectsPercentage : byte { Proyecto1_C = 40, Proyecto2_C = 60 };
+	enum ProductionProjectsPercentage : byte { Proyecto1_P = 15, Proyecto2_P = 30, Proyecto3_P = 30, Proyecto4_P = 25 };
+	enum SalesProjectsPercentage : byte { Proyecto3_V = 100 };
+
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			string userEntry;
+			decimal budget;
+
+			string marketingBudgetOutput;
+			string qnaBudgetOutput;
+			string productionBudgetOutput;
+			string salesBudgetOutput;
+
+			for()
+
+
+			userEntry = Console.ReadLine();
+			budget = Math.Round(Convert.ToDecimal(userEntry), 2);
+
+			string output = @"
+Presupuesto Inicial: {0}
+
+  Presupuesto {} (20%): {1}
+  Presupuesto {} (20%): {1}
+  Presupuesto {} (40%): {1}
+  Presupuesto Mercadotecnia (20%): {1}
+
+			";
+
+			string finalOutput = string.Format(output, asd); 
+			Console.WriteLine(budget);
+		}
+	}
+}
